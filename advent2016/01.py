b10557ab09f9385d890712300a1e0d8b275eb23d
Talha Zaman <talha.zaman@tuebingen.mpg.de>
2016-12-05 17:36:45 +0100

    cleanup

--- advent2016/01.py
+++ advent2016/01.py
@@ -1,41 +1,32 @@
+from collections import namedtuple
 
-def solve(directions):
-    ways = ((1, 0), (0, 1), (-1, 0), (0, -1))
-    position = [0, 0]
-    orientation = 0
+Vec = namedtuple('Vec', 'x y')
+add = lambda v1, v2: Vec(v1.x + v2.x, v1.y + v2.y)
+ways = (Vec(1, 0), Vec(0, 1), Vec(-1, 0), Vec(0, -1))
 
-    for d in directions:
-        orientation += 1 if d[0]=='R' else -1
-        orientation %= 4
-        position[0] += ways[orientation][0] * int(d[1:])
-        position[1] += ways[orientation][1] * int(d[1:])
-
-    return abs(position[0]) + abs(position[1])
-
-def second(directions):
-    ways = ((1, 0), (0, 1), (-1, 0), (0, -1))
-    position = [0, 0]
-    saved = set()
+def solve(directions, part2=False):
+    position = Vec(0, 0)
+    saved = {position}
     orientation = 0
 
     for d in directions:
         orientation += 1 if d[0]=='R' else -1
         orientation %= 4
         for step in range(int(d[1:])):
-            position[0] += ways[orientation][0]
-            position[1] += ways[orientation][1]
-            if tuple(position) in saved:
-                return abs(position[0]) + abs(position[1])
-            saved.add(tuple(position))
-    
+            position = add(position, ways[orientation])
+            if part2 and position in saved:
+                return abs(position.x) + abs(position.y)
+            saved.add(position)
+
+    return abs(position.x) + abs(position.y)
 
 
 if __name__ == '__main__':
-    directions = [d.strip(',') for d in open('1.input').read().split()]
+    directions = open('01.input').read().split(', ')
     assert solve(['R2', 'L3']) == 5
     assert solve(['R2', 'R2', 'R2']) == 2
     assert solve(['R5', 'L5', 'R5', 'R3']) == 12
     print(solve(directions))
 
-    assert second(['R8', 'R4', 'R4', 'R8']) == 4
-    print(second(directions))
+    assert solve(['R8', 'R4', 'R4', 'R8'], part2=True) == 4
+    print(solve(directions, part2=True))
